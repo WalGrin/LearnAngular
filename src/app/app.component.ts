34cd3e44ad23f9bc47d9144412c6201fa3437cad
Waleron <your_email@whatever.com>
2021-02-09 21:10:05 +0300

    pipesw

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   p: Promise<string> = new Promise<string>((resolve) => {
     setTimeout(() => {
       resolve('Promise Resolved');
     }, 3000);
   });
+
+  date$: Observable<Date> = new Observable((obs) => {
+    setInterval(() => {
+      obs.next(new Date());
+    }, 1000);
+  });
+
+  date: Date;
+
+  ngOnInit(): void {
+    this.date$.subscribe((date) => {
+      this.date = date;
+    });
+  }
 }
