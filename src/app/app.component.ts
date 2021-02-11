48363a32c3996557bd4cefd8b3fd19534d2e928d
Waleron <your_email@whatever.com>
2021-02-11 19:08:07 +0300

    finishForms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,59 +1,14 @@
-import { MyValidators } from './my.validators';
-import { Component, OnInit } from '@angular/core';
-import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component } from '@angular/core';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-  form: FormGroup;
+export class AppComponent {
+  appState = 'on';
 
-  ngOnInit() {
-    this.form = new FormGroup({
-      email: new FormControl(
-        '',
-        [Validators.email, Validators.required, MyValidators.restrictedEmails],
-        MyValidators.uniqEmail // асинхронные передаются третьим параметром
-      ),
-      password: new FormControl(null, [
-        Validators.required,
-        Validators.minLength(6),
-      ]),
-      adress: new FormGroup({
-        country: new FormControl('ru'),
-        city: new FormControl('', Validators.required),
-      }),
-      skills: new FormArray([]),
-    });
-  }
-
-  submit() {
-    if (this.form.valid) {
-      console.log('Form: ', this.form);
-      const formData = { ...this.form.value };
-
-      console.log('Form Data: ', formData);
-
-      this.form.reset(); // Очистка формы
-    }
-  }
-
-  setCapital() {
-    const cityMap = {
-      ru: 'Москва',
-      ua: 'Киев',
-      by: 'Минск',
-    };
-
-    const city = cityMap[this.form.get('adress').get('country').value];
-    this.form.patchValue({
-      adress: { city: city },
-    });
-  }
-
-  addSkill() {
-    const control = new FormControl('', Validators.required);
-    (this.form.get('skills') as FormArray).push(control);
+  handleChange() {
+    console.log(this.appState);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,9 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { SwitchComponent } from './switch/switch.component';
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, SwitchComponent],
   imports: [BrowserModule, FormsModule, CommonModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
